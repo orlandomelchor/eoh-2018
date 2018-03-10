60332dfabbd81086b04a0c30ccadf991c9aa2598
Jonathan Richman <cylindricalcow@gmail.com>
2018-03-10 11:10:32 -0600

    Fixed

--- harmonic_finder.py
+++ harmonic_finder.py
@@ -6,7 +6,6 @@ import pandas as pd
 from scipy import stats
 import pickle
 from scipy.optimize import curve_fit
-import time
 from scipy.signal import savgol_filter
 from scipy import asarray as ar,exp
 
@@ -16,10 +15,20 @@ def gaus(x,a,x0,sigma):
 #finds n number of harmonics
 #spacing is the width of the peak
 def peaks(arr, n, spacing=10):
-    L=np.argsort(-arr)
+    arr=np.array(arr)
+    L=np.argsort(-1*arr)
     bad_idx=[]
     for i in range(len(L)-1):
         if abs(L[i]-L[i+1])<spacing:
             bad_idx.append(i+1)
     L=np.delete(L,bad_idx)
-    return np.array(arr[L[:n]])
+    peaks=L[:n]
+    return peaks
+
+test=[np.cos(t) for t in range(0,25)]
+plt.plot(range(0,25), test)
+plt.xticks(range(0,25))
+plt.savefig("test.png")
+plt.close()
+
+print(peaks(test, 4, 2))
