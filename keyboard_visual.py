357a9d74255d501917d0c3022756e9b79a4ca5e2
PhzWhz <habibsarah11@gmail.com>
2018-03-10 08:22:24 -0600

    Bug fixing and added legend

--- keyboard_visual.py
+++ keyboard_visual.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from math import pi, sin
-#import soundtest as st
+import soundtest as st
 
 xdata = np.linspace(0, 2*np.pi, 60000)
 j = np.size(xdata)
@@ -10,14 +10,13 @@ j = np.size(xdata)
 xdata = np.reshape(xdata, (1, j))
 fig, ax = plt.subplots()
 
-#ln, = plt.plot([], [], 'ro', animated=True)
 def update(frame):
 	ax.clear()
 	#A Major Chord exhibit (A, C#, E)
-	#f = np.array([440.0, 554.37, 659.25])
+	#f,name = np.array([440.0, 554.37, 659.25]), ["A","C#","E"]
 	#Microphone input exhibit
 	f,name = st.sample_sound()
-	f = np.array(f)
+	f = np.array([f])
 
 	i=np.size(f)
 	f = np.reshape(f, (1, i))
@@ -25,14 +24,13 @@ def update(frame):
 	actual = np.zeros(j)
 	for k in range(i):
 		curve = np.sin(curve_arr[k])
-		ax.plot(xdata[0], curve)
+		ax.plot(xdata[0], curve, label=name + " (freq: " + str(int(f[0,k])) + ")")
 		actual +=  curve
 	if (i > 1):
 		ax.plot(xdata[0], actual)
 	ax.set_xlim(0, np.pi/50)
 	ax.set_ylim(-5, 5)
-#    ln.set_data(xdata, ydata)
-#    return ln,
+	plt.legend()
 
 ani = FuncAnimation(fig, update, interval=50)
 plt.show()
