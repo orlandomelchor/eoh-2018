7a298bed2f6677fed218e026bcf3cd0c0c01ae06
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2018-03-10 09:05:09 -0600

    multiple curves superimposed

--- keyboard_visual.py
+++ keyboard_visual.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from math import pi, sin
-import soundtest as st
+#import soundtest as st
 
 xdata = np.linspace(0, 2*np.pi, 60000)
 j = np.size(xdata)
@@ -13,10 +13,10 @@ fig, ax = plt.subplots()
 def update(frame):
 	ax.clear()
 	#A Major Chord exhibit (A, C#, E)
-	#f,name = np.array([440.0, 554.37, 659.25]), ["A","C#","E"]
+	f, name = np.asarray([440.0, 554.37, 659.25]), ["A","C#","E"]
 	#Microphone input exhibit
-	f,name = st.sample_sound()
-	f = np.array([f])
+	#f, name = st.sample_sound()
+	f = np.asarray(f)
 
 	i=np.size(f)
 	f = np.reshape(f, (1, i))
@@ -24,10 +24,11 @@ def update(frame):
 	actual = np.zeros(j)
 	for k in range(i):
 		curve = np.sin(curve_arr[k])
-		ax.plot(xdata[0], curve, label=name + " (freq: " + str(int(f[0,k])) + ")")
+		ax.plot(xdata[0], curve, label=name[k] + " (freq: " + str(int(f[0,k])) + ")")
 		actual +=  curve
 	if (i > 1):
 		ax.plot(xdata[0], actual)
+		ax.plot(xdata[0], actual, label="superposition")
 	ax.set_xlim(0, np.pi/50)
 	ax.set_ylim(-5, 5)
 	plt.legend()
