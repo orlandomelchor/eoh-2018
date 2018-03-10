83b22c3784568de5086ecc7fd8149b0913755a87
PhzWhz <habibsarah11@gmail.com>
2018-03-10 04:26:36 -0600

    moving sine wave

--- visual.py
+++ visual.py
@@ -1,22 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import math
 
 fig, ax = plt.subplots()
-xdata, ydata = [], []
-ln, = plt.plot([], [], 'ro', animated=True)
-
-def init():
-    ax.set_xlim(0, 2*np.pi)
-    ax.set_ylim(-1, 1)
-    return ln,
+xdata = np.linspace(0, 2*np.pi, 200)
+ydata = np.sin(xdata)
+ax.plot(xdata, ydata)
+#ln, = plt.plot([], [], 'ro', animated=True)
 
 def update(frame):
-    xdata.append(frame)
-    ydata.append(np.sin(frame))
-    ln.set_data(xdata, ydata)
-    return ln,
+	ydata = np.sin(xdata + frame)
+	ax.clear()
+	ax.plot(xdata, ydata)
+#    ln.set_data(xdata, ydata)
+#    return ln,
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
-                    init_func=init, blit=True)
+ani = FuncAnimation(fig, update)
 plt.show()
